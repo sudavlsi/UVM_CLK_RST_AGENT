bbf431b9bd184fc63de0c1b043852a94b466be8d
suda7975 <sudarshanavlsi@gmail.com>
2025-03-30 21:02:00 +0530

    added timescale and cfg agent files

--- clk_rst_pkg.svh
+++ clk_rst_pkg.svh
@@ -1,4 +1,11 @@
+`ifndef CLK_RST_PKG
+	`define CLK_RST_PKG
 package clk_rst_pkg;
+	timeunit 1ns;
+	timeprecision 1ns;
+	import uvm_pkg::*;
+	`include "uvm_macros.svh"
 	`include "clk_rst_cfg.sv"
 	`include "clk_rst_agent.sv"
 endpackage
+`endif
