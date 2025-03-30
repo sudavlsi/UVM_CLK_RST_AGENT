6068884aeb0d31bbc8b8c57d540ba12e51fe446a
suda7975 <sudarshanavlsi@gmail.com>
2025-03-30 21:00:23 +0530

    added clk rst cfg , updated clk gen logic with fork join_none , get clk rst intf

--- clk_rst_agent.sv
+++ clk_rst_agent.sv
@@ -1,10 +1,7 @@
-	//import Riscv_defines_tb_pkg::*;
-`include "uvm_macros.svh"
-import uvm_pkg::*;
 class clk_rst_agent extends uvm_agent ;
 `uvm_component_utils(clk_rst_agent)
-  virtual clk_rst_intf clk_rst_vif[NUM_CLKS];
-  int clk_period[NUM_CLKS];
+  clk_rst_cfg cfg;
+  virtual clk_rst_intf clk_rst_vif[];
 
   function new (string name, uvm_component parent);
     super.new(name, parent);
@@ -12,55 +9,46 @@ class clk_rst_agent extends uvm_agent ;
 
    function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-	 for(int clk_idx=0 ; clk_idx < NUM_CLKS ; clk_idx ++)	begin
+	     cfg = clk_rst_cfg::type_id::create("cfg");
+	if(!cfg.randomize()) `uvm_fatal(get_type_name(),"CLK RST CFG RANDOMIZE FAILURE")
+	`uvm_info(get_type_name(),$sformatf("num clocks %0d",cfg.num_clks),UVM_NONE)
+	clk_rst_vif = new[cfg.num_clks];	
+	 for(int clk_idx=0 ; clk_idx < cfg.num_clks ; clk_idx ++)	begin
     if(!uvm_config_db#(virtual clk_rst_intf)::get(this,"",$sformatf("clk_rst_intf_%0d",clk_idx),clk_rst_vif[clk_idx]))
-      `uvm_fatal(get_type_name(),"DRIVER FAILED TO GET AXI MASTER INTF")
+      `uvm_fatal(get_type_name(),"DRIVER FAILED TO GET CLK RST INTF")
+ 
 	 end 
   endfunction
  
 task run_phase(uvm_phase phase);
 super.run_phase(phase);
+
 	main();
 endtask
 
 task main(); 
-//fork
-	for (int clk_idx=0 ; clk_idx < NUM_CLKS ; clk_idx++) 
+	for (int clk_idx=0 ; clk_idx < cfg.num_clks ; clk_idx++) 
 		fork
-			clk_gen(clk_idx);
-			reset_gen(clk_idx);
-		join
+			automatic int clk_jdx = clk_idx;
+			clk_gen(clk_jdx);
+			reset_gen(clk_jdx);
+		join_none
+		wait fork;
 	
-//join
 endtask
    task automatic reset_gen(int clk_idx);
     	int count,on_delay,off_delay;
        on_delay  = $urandom_range(10,100);
-     $display("SUDA RST 59");
+     	 $display("SUDA RST 59");
        clk_rst_vif[clk_idx].rstN = 0;
        #(on_delay) clk_rst_vif[clk_idx].rstN = 1;
      $display("%0tSUDA RST 62",$time);
   endtask
   
-//    task uart_reset_gen();
-//    	int count,on_delay,off_delay;
-//       on_delay  = $urandom_range(10,100);
-//       clk_rst_intf.uart_rstN = 0;
-//      #(on_delay) clk_rst_intf.uart_rstN = 1;
-//  endtask
   
   task clk_gen(int clk_idx);
+	 $display("period %p",real'(real'(cfg.clk_period[clk_idx])/2));
 	 clk_rst_vif[clk_idx].clk = 0;
-    forever #(clk_period[clk_idx]/2) clk_rst_vif[clk_idx].clk =~clk_rst_vif[clk_idx].clk;
+    forever #(real'(real'(cfg.clk_period[clk_idx])/2)) clk_rst_vif[clk_idx].clk =~clk_rst_vif[clk_idx].clk;
   endtask  
-//	task uart_clk_gen();
-//int clk_period,skew;
-//clk_period = $urandom_range(2,20);
-//skew = $urandom_range(0,9);
-//clk_rst_intf.uart_clk = 0;
-//#(skew);
-//forever #(clk_period/2) clk_rst_intf.uart_clk =~clk_rst_intf.uart_clk;
-//
-//
-//endtask
 endclass
